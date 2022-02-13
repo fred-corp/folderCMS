592df96e1530cd723e8286ac203202ef436aa0e9
fred-corp <freddruppel@gmail.com>
2022-02-14 00:09:29 +0100

    added markdown parsing capabilities

--- controllers/mainSiteController.js
+++ controllers/mainSiteController.js
@@ -1,6 +1,9 @@
 const config = require("../server-config.json")
 const mainSite = require('../models/mainSiteModel')
+const fs = require('fs')
+const marked = require('marked')
 
 exports.homePage = function (req, res) {
-  res.render('mainSite.ejs', {siteTitle: config.siteTitle, siteName: config.siteName, theme: config.theme})
+  let html = marked.parse(fs.readFileSync('website/01.Home.md').toString())
+  res.render('mainSite.ejs', {siteTitle: config.siteTitle, theme: config.theme, siteName: config.siteName, content: html})
 }
