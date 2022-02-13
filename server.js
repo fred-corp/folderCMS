916971ded64043e9af29fa9afac8423ecd32ae3d
fred-corp <freddruppel@gmail.com>
2022-02-13 22:39:08 +0100

    added config file, closes #8

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const app = express()
 const routes = require('./routes')
-
-const port = 3000
+const config = require("./server-config.json")
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
 app.use('/', routes)
 
-app.listen(port, function () {
-  console.log('server is listening on port ' + port)
+app.listen(config.port, function () {
+  console.log('server is listening on port ' + config.port)
 })
