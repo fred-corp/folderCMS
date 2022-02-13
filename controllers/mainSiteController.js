b101a96d1e99ca9f5496e17a2c2c2331b824b299
fred-corp <freddruppel@gmail.com>
2022-02-13 23:25:05 +0100

    added parameter to config file #8

--- controllers/mainSiteController.js
+++ controllers/mainSiteController.js
@@ -2,5 +2,5 @@ const config = require("../server-config.json")
 const mainSite = require('../models/mainSiteModel')
 
 exports.homePage = function (req, res) {
-  res.render('mainSite.ejs', {siteName: config.siteName, theme: config.theme})
+  res.render('mainSite.ejs', {siteTitle: config.siteTitle, siteName: config.siteName, theme: config.theme})
 }
