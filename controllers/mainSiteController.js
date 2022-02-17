a3ab551ce1dd93281d78113f9bae1898d1e4a520
fred-corp <freddruppel@gmail.com>
2022-02-17 13:33:30 +0100

    #9 cleaned up code #TODO : generate navBarDict from webpage folder

--- controllers/mainSiteController.js
+++ controllers/mainSiteController.js
@@ -3,8 +3,9 @@ const mainSite = require('../models/mainSiteModel')
 const fs = require('fs')
 const marked = require('marked')
 
+const navBarDict = {active: 0, right: 2, items: [["page", "Home", "/"], ["page", "Test", "/test"], ["page", "About", "/about"]]}
+
 exports.homePage = function (req, res) {
   let html = marked.parse(fs.readFileSync('website/01.Home.md').toString())
-  const navBarDict = {active: 0, right: 2, items: [["page", "Home", "/"], ["page", "Test", "/test"], ["page", "About", "/about"]]}
   res.render('mainSite.ejs', {navBar: navBarDict, siteTitle: config.siteTitle, theme: config.theme, siteName: config.siteName, content: html})
 }
