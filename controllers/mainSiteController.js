c3f8b563f952b1bece8656d9a03d06dc55566410
fred-corp <freddruppel@gmail.com>
2022-02-17 13:40:04 +0100

    #12 rearranged website folder

--- controllers/mainSiteController.js
+++ controllers/mainSiteController.js
@@ -6,6 +6,6 @@ const marked = require('marked')
 const navBarDict = {active: 0, right: 2, items: [["page", "Home", "/"], ["page", "Test", "/test"], ["page", "About", "/about"]]}
 
 exports.homePage = function (req, res) {
-  let html = marked.parse(fs.readFileSync('website/01.Home.md').toString())
+  let html = marked.parse(fs.readFileSync('website/01.Home/page.md').toString())
   res.render('mainSite.ejs', {navBar: navBarDict, siteTitle: config.siteTitle, theme: config.theme, siteName: config.siteName, content: html})
 }
