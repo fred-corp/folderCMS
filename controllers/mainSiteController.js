255fe894b90a62d1d822cff8e3c4242f0e3d6606
fred-corp <freddruppel@gmail.com>
2022-02-17 13:56:33 +0100

    applied standard linting

--- controllers/mainSiteController.js
+++ controllers/mainSiteController.js
@@ -1,11 +1,11 @@
-const config = require("../server-config.json")
+const config = require('../server-config.json')
 const mainSite = require('../models/mainSiteModel')
 const fs = require('fs')
 const marked = require('marked')
 
-const navBarDict = {active: 0, right: 2, items: [["page", "Home", "/"], ["page", "Test", "/test"], ["page", "About", "/about"]]}
+const navBarDict = { active: 0, right: 2, items: [['page', 'Home', '/'], ['page', 'Test', '/test'], ['page', 'About', '/about']] }
 
 exports.homePage = function (req, res) {
-  let html = marked.parse(fs.readFileSync('website/01.Home/page.md').toString())
-  res.render('mainSite.ejs', {navBar: navBarDict, siteTitle: config.siteTitle, theme: config.theme, siteName: config.siteName, content: html})
+  const html = marked.parse(fs.readFileSync('website/01.Home/page.md').toString())
+  res.render('mainSite.ejs', { navBar: navBarDict, siteTitle: config.siteTitle, theme: config.theme, siteName: config.siteName, content: html })
 }
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 const routes = require('./routes')
-const config = require("./server-config.json")
+const config = require('./server-config.json')
 const fs = require('fs')
-var path = require('path')
+const path = require('path')
 
 const websiteDir = config.websiteDir
 
@@ -16,27 +16,27 @@ app.listen(config.port, function () {
   console.log('server is listening on port ' + config.port)
 })
 
-function searchPages() {
-  const mdFiles = getFilesFromDir("website", [".md"])
+function searchPages () {
+  const mdFiles = getFilesFromDir('website', ['.md'])
   console.log(mdFiles)
 }
 
 // function from https://www.codexpedia.com/node-js/node-js-getting-files-from-a-directory-including-sub-directories/
-function getFilesFromDir(dir, fileTypes) {
-  var filesToReturn = [];
-  function walkDir(currentPath) {
-    var files = fs.readdirSync(currentPath);
-    for (var i in files) {
-      var curFile = path.join(currentPath, files[i]);      
+function getFilesFromDir (dir, fileTypes) {
+  const filesToReturn = []
+  function walkDir (currentPath) {
+    const files = fs.readdirSync(currentPath)
+    for (const i in files) {
+      const curFile = path.join(currentPath, files[i])
       if (fs.statSync(curFile).isFile() && fileTypes.indexOf(path.extname(curFile)) != -1) {
-        filesToReturn.push(curFile.replace(dir, ''));
+        filesToReturn.push(curFile.replace(dir, ''))
       } else if (fs.statSync(curFile).isDirectory()) {
-        walkDir(curFile);
+        walkDir(curFile)
       }
     }
   };
-  walkDir(dir);
-  return filesToReturn; 
+  walkDir(dir)
+  return filesToReturn
 }
 
-setInterval(searchPages, config.refreshInterval*1000)
+setInterval(searchPages, config.refreshInterval * 1000)
