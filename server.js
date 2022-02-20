faa9ab8729c78976bf5645cadbd12210c5bad1f9
fred-corp <freddruppel@gmail.com>
2022-02-20 17:53:37 +0100

    removed unnessecary requirements

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require('express')
 const app = express()
 const routes = require('./routes')
 const config = require('./server-config.json')
-const fs = require('fs')
-const path = require('path')
-const directoryTree = require('directory-tree')
 const { connected } = require('process')
 
 const websiteDir = config.websiteDir
