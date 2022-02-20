b5804df22d1139301e0948b2c21393ce9808bcc4
fred-corp <freddruppel@gmail.com>
2022-02-20 02:26:04 +0100

    #17 wrote a method for generating a navBarDict

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const routes = require('./routes')
 const config = require('./server-config.json')
 const fs = require('fs')
 const path = require('path')
+const directoryTree = require('directory-tree')
+const { connected } = require('process')
 
 const websiteDir = config.websiteDir
 
@@ -16,29 +18,54 @@ app.listen(config.port, function () {
   console.log('server is listening on port ' + config.port)
 })
 
-function searchPages () {
-  const mdFiles = getFilesFromDir(websiteDir, ['.md'])
-  const jsonFiles = getFilesFromDir(websiteDir, ['.json'])
-  console.log(mdFiles)
-  console.log(jsonFiles)
-}
 
-// function from https://www.codexpedia.com/node-js/node-js-getting-files-from-a-directory-including-sub-directories/
-function getFilesFromDir (dir, fileTypes) {
-  const filesToReturn = []
-  function walkDir (currentPath) {
-    const files = fs.readdirSync(currentPath)
-    for (const i in files) {
-      const curFile = path.join(currentPath, files[i])
-      if (fs.statSync(curFile).isFile() && fileTypes.indexOf(path.extname(curFile)) != -1) {
-        filesToReturn.push(curFile.replace(dir, ''))
-      } else if (fs.statSync(curFile).isDirectory()) {
-        walkDir(curFile)
+// TODO : add folder capability to navbar item
+function searchPages () {
+  var website = directoryTree(config.websiteDir, {attributes:['type', 'extension'], normalizePath:true})
+  
+  var navBarDict = {}
+  navBarDict.active = 0
+  navBarDict.items = []
+
+  function getInfo(element) {
+    if(element.hasOwnProperty('children')) {
+      for (let index = 0; index < element.children.length; index++) {
+        const child = element.children[index]
+        getInfo(child)
+      }
+    } else {
+      parseElement(element)
+    }
+  }
+
+  function parseElement(element){
+    const subnames = element.path.split('/')
+    console.log(subnames)
+    if(subnames.length <= 3) {
+      const index_name = subnames[subnames.length-2].split('.')
+      if (element.extension == '.md') {
+        navBarDict.items[parseInt(index_name[0]) - 1].type = 'page' 
+        navBarDict.items[parseInt(index_name[0]) - 1].name = index_name[1]
+        navBarDict.items[parseInt(index_name[0]) - 1].pageFile = element.path
+      } else if (element.extension == '.json') {
+        const conf = require('./' + element.path)
+        navBarDict.items[parseInt(index_name[0]) - 1] = {}
+        navBarDict.items[parseInt(index_name[0]) - 1].URL = conf.URL
+        navBarDict.items[parseInt(index_name[0]) - 1].float = conf.navbar.desktop.float
       }
     }
-  };
-  walkDir(dir)
-  return filesToReturn
+  }
+
+  for (let index = 0; index < website.children.length; index++) {
+    const child = website.children[index]
+    getInfo(child)
+    
+  }
+
+  console.log(navBarDict)
+
+  // Now to send this dictionary somewhere...
+
 }
 
 setInterval(searchPages, config.refreshInterval<1 ? 1000 : config.refreshInterval * 1000)
