d5d791ad30c8abb395df3941e031b8311b073113
fred-corp <freddruppel@gmail.com>
2022-02-20 18:07:43 +0100

    code cleanup with standard

--- controllers/mainSiteController.js
+++ controllers/mainSiteController.js
@@ -1,55 +1,52 @@
 const config = require('../server-config.json')
-const mainSiteModel = require('../models/mainSiteModel')
+// const mainSiteModel = require('../models/mainSiteModel')
 const fs = require('fs')
-const path = require('path')
+// const path = require('path')
 const directoryTree = require('directory-tree')
 const marked = require('marked')
 
-const navBarDict = { active: 0, items: [{type:'page', name:'Home', URL:'/', path:'website/01.Home/', float:'none'}, {type:'page', name:'Test', URL:'/test', path:'website/01.Home/'}, {type:'page', name:'page', name:'About', URL:'/about', path:'website/03.About/'}] }
+// const navBarDict = { active: 0, items: [{ type: 'page', name: 'Home', URL: '/', path: 'website/01.Home/', float: 'none' }, { type: 'page', name: 'Test', URL: '/test', path: 'website/01.Home/' }, {type: 'page', name: 'About', URL: '/about', path: 'website/03.About/' }] }
 
 exports.homePage = function (req, res) {
   const html = marked.parse(fs.readFileSync('website/01.Home/page.md').toString())
   res.render('mainSite.ejs', { navBar: searchPages(), config: config, content: html })
 }
 
-
 function searchPages () {
-  var website = directoryTree(config.websiteDir, {attributes:['type', 'extension'], normalizePath:true})
-  
-  var navBarDict = {}
+  const website = directoryTree(config.websiteDir, { attributes: ['type', 'extension'], normalizePath: true })
+
+  const navBarDict = {}
   navBarDict.active = 0
   navBarDict.items = []
 
-  function getInfo(element) {
-    if(element.hasOwnProperty('children')) {
+  function getInfo (element) {
+    if (Object.prototype.hasOwnProperty.call(element, 'children')) {
       const conf = require('../' + element.path + '/conf.json')
-      if(conf.type == 'page') {
+      if (conf.type === 'page') {
         return parseElement(element)
-      } 
-      else if (conf.type == 'folder') {
-        let item = parseElement(element)
-        var items = []
+      } else if (conf.type === 'folder') {
+        const item = parseElement(element)
+        const items = []
         for (let index = 0; index < element.children.length; index++) {
           const child = element.children[index]
-          let subItem = getInfo(child)
+          const subItem = getInfo(child)
           if (typeof subItem !== 'undefined') {
             items.push(subItem)
           }
         }
         item.subpages = items
-        
+
         return item
       }
     }
   }
 
-  function parseElement(element){
-    var item = {}
+  function parseElement (element) {
+    const item = {}
     const subnames = element.path.split('/')
-    const index_name = subnames[subnames.length-1].split('.')
-    item = {}
-    item.name = index_name[1]
-    item.index = parseInt(index_name[0])
+    const indexAndName = subnames[subnames.length - 1].split('.')
+    item.name = indexAndName[1]
+    item.index = parseInt(indexAndName[0])
     item.path = element.path
     const conf = require('../' + element.path + '/conf.json')
     item.type = conf.type
@@ -58,7 +55,7 @@ function searchPages () {
     return item
   }
 
-  var items = []
+  const items = []
   for (let index = 0; index < website.children.length; index++) {
     const child = website.children[index]
     items.push(getInfo(child))
--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const routes = require('./routes')
 const config = require('./server-config.json')
-const { connected } = require('process')
+// const { connected } = require('process')
 
-const websiteDir = config.websiteDir
+// const websiteDir = config.websiteDir
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
