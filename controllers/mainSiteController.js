87a9acf0ec868345854800ed9549516fee12fb38
fred-corp <freddruppel@gmail.com>
2022-02-20 02:28:53 +0100

    #17 changed key in navBardict

--- controllers/mainSiteController.js
+++ controllers/mainSiteController.js
@@ -3,7 +3,7 @@ const mainSiteModel = require('../models/mainSiteModel')
 const fs = require('fs')
 const marked = require('marked')
 
-const navBarDict = { active: 0, items: [{type:'page', name:'Home', URL:'/', pageFile:'website/01.Home/page.md', float:'none'}, {type:'page', name:'Test', URL:'/test', pageFile:'website/01.Home/page.md'}, {type:'page', name:'page', name:'About', URL:'/about', pageFile:'website/03.About/page.md'}] }
+const navBarDict = { active: 0, items: [{type:'page', name:'Home', URL:'/', path:'website/01.Home/', float:'none'}, {type:'page', name:'Test', URL:'/test', path:'website/01.Home/'}, {type:'page', name:'page', name:'About', URL:'/about', path:'website/03.About/'}] }
 
 exports.homePage = function (req, res) {
   const html = marked.parse(fs.readFileSync('website/01.Home/page.md').toString())
