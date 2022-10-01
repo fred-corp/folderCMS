61e491d294d53c57d86385c54cca046411cf94c3
fred-corp <7109145+fred-corp@users.noreply.github.com>
2022-10-01 20:28:01 +0200

    redirct / to /Home, and parse URL params

--- routes.js
+++ routes.js
@@ -3,6 +3,12 @@ const router = express.Router()
 
 const mainSiteController = require('./controllers/mainSiteController')
 
-router.get('/', mainSiteController.homePage)
+// redirect "/"" to "/home"
+router.get('/', function (req, res) {
+  res.redirect('/Home')
+})
+
+// redirect all trafic to mainsiteController.getPage
+router.get('/:page', mainSiteController.getPage)
 
 module.exports = router
