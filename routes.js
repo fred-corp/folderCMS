99b80f396c728f9002016690a15f9496ab5c89a3
fred-corp <7109145+fred-corp@users.noreply.github.com>
2022-10-02 01:17:39 +0200

    redirect traffic, serve the .ico and .css files

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const config = require('./server-config.json')
 
 const mainSiteController = require('./controllers/mainSiteController')
 
@@ -8,7 +9,20 @@ router.get('/', function (req, res) {
   res.redirect('/Home')
 })
 
-// redirect all trafic to mainsiteController.getPage
-router.get('/:page', mainSiteController.getPage)
+// redirect all URLs that are not .ico and .css files to mainsiteController.getPage
+router.get('/*', function (req, res) {
+  if (req.params['0'].endsWith('.ico')){
+    // get only the filename from the URL
+    const filename = req.params['0'].split('/').pop()
+    res.sendFile(filename, { root: config.websiteDir+'/images' })
+
+  } else if (req.params['0'].endsWith('.css')) {
+    // get only the filename from the URL
+    const filename = req.params['0'].split('/').pop()
+    res.sendFile('themes/' + filename, { root: 'public' })
+  } else {
+    mainSiteController.getPage(req, res)
+  }
+})
 
 module.exports = router
